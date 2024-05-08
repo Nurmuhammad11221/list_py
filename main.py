1920bd3aba373577cc39b0ab56a5229fa3ba2e3e
Nurmuhammad11221 <roziyaabdurazoqova@gmail.com>
2024-05-08 13:18:05 +0500

    second commit

---
+++ main.py
@@ -0,0 +1,89 @@
+# cars = []
+
+# cars.append("bmw")
+# cars.append("ford")
+# cars.append("mustang")
+# cars.append("chevrolet")
+# cars.append("tesla")
+# # cars[4] = "byd"
+# cars.insert(4, "byd")
+# del cars [1]
+# cars.remove("tesla")
+# print(cars)                    
+
+# animals = ["cat", "dog", "cow", "rabbit"]
+
+# animal = animals.pop(2)
+
+# print(f"Menda {animal} bor")
+# print(f"Menda bu {animals} yoq")
+
+"Amalyot "
+
+# ismlar = ["Muzaffar", "Rizo", "bot"]
+# name = ismlar.pop(1)
+
+# print(f"Salom {name}, bugun choyxona bormi?")   
+
+# sonlar = ["10", "-11", "0.5"]
+# # print(f"{sonlar[0]} + {sonlar[1]} = ")
+
+# add = sonlar[2] + sonlar [3]
+
+# division = sonlar[3] / sonlar[2]
+# print(add)
+# print(division)
+# sonlar[4] = -23
+
+# sonlar[5] = division
+
+# print(sonlar)
+
+# t_shaxslar = ["Imom Buxuriy", "Alsher Navoiy", "Xorazimiy"]
+# z_shaxslar = ["Ilon mask", "Bill Gates", "Yakudza"]
+# imom = t_shaxslar.pop(0)
+# ilon = z_shaxslar.pop(0)
+# print(f"Men tarixiy shaxlardan {imom} bilan,\n zamonaviy shaxslardan esa {ilon} bilan suhbat qilishni istar edim")
+
+
+friends = []
+
+friends.append("Rizo")
+friends.append("Muzap")
+friends.append("Habi")
+friends.append("Jovo")
+friends.append("Mustaf")
+
+friends.remove("Rizo")
+friends.remove("Muzap")
+
+
+
+friends.insert(0, "SHoxrux")
+friends.insert(6, "Humoyn")
+
+mehmonlar = []
+
+f1 = friends.pop(-1)
+f2 = friends.pop(-2)
+f3 = friends.pop(-3)
+
+mehmonlar.append(f1)
+mehmonlar.append(f2)
+mehmonlar.append(f3)
+
+print(mehmonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
